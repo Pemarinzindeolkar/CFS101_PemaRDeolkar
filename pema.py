0f25cf69be5ecac2599eaf4668a54a28f72b006f
Pema_Rinzin_Deolkar <deolkar07@gmail.com>
2025-07-29 14:35:14 +0600

    class commit

--- pema.py
+++ pema.py
@@ -2,4 +2,10 @@ p = 7
 r = 2
 d = 4
 sum = p + r + d
-print(sum)
+print(" The total sum is", sum)
+
+
+name = input("Enter name: ")
+age = input("Enter age: ")
+
+print("hello", name, "You are", age)
