bd112ccbe5150cf0ce978dcc7b30be335746aeee
Pema_Rinzin_Deolkar <deolkar07@gmail.com>
2025-08-12 14:53:39 +0600

    merge earlier codes

--- first_programme.py
+++ first_programme.py
@@ -1,2 +1,14 @@
 print ("hello Pema!")
 print ("Cheers to new beginnings!")
+
+p = 7
+r = 2
+d = 4
+sum = p + r + d
+print(" The total sum is", sum)
+
+
+name = input("Enter name: ")
+age = input("Enter age: ")
+
+print("hello", name, "You are", age)
